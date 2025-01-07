075627b2f6563c4db832b0bbc51aaa74b642b04a
casdorio <casdorio@gmail.com>
2025-01-07 12:27:56 -0300

    Deprecated: Optional parameter $id declared before required parameter

--- GatewayPayment/Entities/Gateway.php
+++ GatewayPayment/Entities/Gateway.php
@@ -7,7 +7,6 @@ use CodeIgniter\Entity\Entity;
 class Gateway extends Entity
 {
     public function __construct(
-        public ?int $id = null,
         public string $name,
         public string $login_id,
         public string $transaction_key,
